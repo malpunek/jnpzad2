c8e9f24a932c48dfde3e4c32a88aae75a50400cb
Kacper Pawelec <kp332436@students.mimuw.edu.pl>
2016-10-27 22:43:04 +0200

    naprawa na spacje

--- strdeque.h
+++ strdeque.h
@@ -2,33 +2,33 @@
 #define strdeque_h
 
 #ifdef __cplusplus
-	#include<cstddef>
-	#include<iostream>
-	namespace jnp1 {
-		extern "C" {
+    #include<cstddef>
+    #include<iostream>
+    namespace jnp1 {
+        extern "C" {
 #else
-	#include<stddef.h>
+    #include<stddef.h>
 #endif
 
-			unsigned long strdeque_new();
+            unsigned long strdeque_new();
 
-			void strdeque_delete(unsigned long id);
+            void strdeque_delete(unsigned long id);
 
-			size_t strdeque_size(unsigned long id);
+            size_t strdeque_size(unsigned long id);
 
-			void strdeque_insert_at(unsigned long id, size_t pos, const char* value);
+            void strdeque_insert_at(unsigned long id, size_t pos, const char* value);
 
-			void strdeque_remove_at(unsigned long id, size_t pos);
+            void strdeque_remove_at(unsigned long id, size_t pos);
 
-			const char* strdeque_get_at(unsigned long id, size_t pos);
+            const char* strdeque_get_at(unsigned long id, size_t pos);
 
-			void strdeque_clear(unsigned long id);
+            void strdeque_clear(unsigned long id);
 
-			int strdeque_comp(unsigned long id1, unsigned long id2);
+            int strdeque_comp(unsigned long id1, unsigned long id2);
 
 #ifdef __cplusplus
-		}
-	}
+        }
+    }
 #endif
 
 
--- strdequeconst.h
+++ strdequeconst.h
@@ -2,14 +2,14 @@
 #define strdequeconst_h
 
 #ifdef __cplusplus
-	#include <iostream>
-	namespace jnp1{
-		extern "C"
+    #include <iostream>
+    namespace jnp1{
+        extern "C"
 #endif
-			unsigned long emptystrdeque();
+            unsigned long emptystrdeque();
 
 #ifdef __cplusplus
-	}
+    }
 #endif
 
 #endif /* strdequeconst_h */
